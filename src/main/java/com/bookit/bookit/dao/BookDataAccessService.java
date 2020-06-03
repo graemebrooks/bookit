5b4ca2166282fabb7c537e27864ed2fc6869a73f
graemebrooks <graemebrooks@gmail.com>
2020-06-03 13:15:05 -0500

    implements postgres read functionality

--- src/main/java/com/bookit/bookit/dao/BookDataAccessService.java
+++ src/main/java/com/bookit/bookit/dao/BookDataAccessService.java
@@ -1,6 +1,8 @@
 package com.bookit.bookit.dao;
 
 import com.bookit.bookit.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,15 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class BookDataAccessService implements BookDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
     @Override
     public int insertBook(UUID id, Book book) {
         return 0;
@@ -16,12 +27,28 @@ public class BookDataAccessService implements BookDao {
 
     @Override
     public List<Book> selectAllBooks() {
-        return List.of(new Book(UUID.randomUUID(), "FROM POSTGRES DB", "JOHN SMITH", "FANTASY"));
+        final String sql = "SELECT id, title, author, genre FROM book";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String title = resultSet.getString("title");
+            String author = resultSet.getString("author");
+            String genre = resultSet.getString("genre");
+            return new Book(id, title, author, genre);
+        });
     }
 
     @Override
     public Optional<Book> selectBookById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, title, author, genre FROM book WHERE id = ?";
+
+        Book book = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet, i) -> {
+            UUID bookId = UUID.fromString(resultSet.getString("id"));
+            String title = resultSet.getString("title");
+            String author = resultSet.getString("author");
+            String genre = resultSet.getString("genre");
+            return new Book(bookId, title, author, genre);
+        });
+        return Optional.ofNullable(book);
     }
 
     @Override
